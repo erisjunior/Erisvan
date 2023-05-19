3bfcc159bd92453427fb87a2dccdf59fb895bfea
Antonio Erisvan <erisvan.junior.a@gmail.com>
2023-05-18 23:00:12 -0300

    fix: ssr background

--- src/components/background/index.tsx
+++ src/components/background/index.tsx
@@ -1,9 +1,20 @@
 "use client";
+
+import { useEffect, useState } from "react";
 import styles from "./styles.module.css";
 
 export function Background() {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
+  const [{ width, height }, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    setWindowSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }, []);
 
   const starsPositions = Array.from({ length: 100 }, () => ({
     x: Math.random() * width,
